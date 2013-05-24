d759013380a1b73fc4314e38783ef1f95a21783e
ProfGerdes <jgerdes@sc.edu>
2013-05-24 10:07:25 -0400

    Add A button

--- Hello World/Hello World/Form1.Designer.vb
+++ Hello World/Hello World/Form1.Designer.vb
@@ -22,9 +22,43 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.lblHello = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(182, 208)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 23)
+        Me.btnGo.TabIndex = 0
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'lblHello
+        '
+        Me.lblHello.AutoSize = True
+        Me.lblHello.Location = New System.Drawing.Point(50, 81)
+        Me.lblHello.Name = "lblHello"
+        Me.lblHello.Size = New System.Drawing.Size(65, 13)
+        Me.lblHello.TabIndex = 1
+        Me.lblHello.Text = "Hello World!"
+        Me.lblHello.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblHello)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+    Friend WithEvents lblHello As System.Windows.Forms.Label
 
 End Class
--- Hello World/Hello World/Form1.vb
+++ Hello World/Hello World/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub btnGo_Click(sender As System.Object, e As System.EventArgs) Handles btnGo.Click
+        lblHello.Visible = True
+    End Sub
 End Class
